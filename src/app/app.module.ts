7875e75fb25fc669f6e541ef674ba095d1895caf
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-11-07 08:17:17 -0200

    Exportation Page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { BsDatepickerModule, BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+import { NgxCurrencyModule } from "ngx-currency";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeneficioService } from './beneficio.service';
 import { BeneficioComponent } from './beneficio/beneficio.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { TextMaskModule } from 'angular2-text-mask';
+
+defineLocale('pt-br', ptBrLocale);
+
+export const customCurrencyMaskConfig = {
+  align: "center",
+  allowNegative: true,
+  allowZero: true,
+  decimal: ",",
+  precision: 2,
+  prefix: "R$ ",
+  suffix: "",
+  thousands: ".",
+  nullable: true
+};
+
 
 @NgModule({
   declarations: [
@@ -19,10 +38,19 @@ import { TextMaskModule } from 'angular2-text-mask';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    TextMaskModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig),
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [BeneficioService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+  constructor(private bsLocaleService: BsLocaleService) {
+    this.bsLocaleService.use('pt-br');
+  }
+
+}
--- src/app/beneficio/beneficio.component.ts
+++ src/app/beneficio/beneficio.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+//import { BsDatepickerConfig, BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { Beneficio } from '../beneficio';
 import { BeneficioService } from '../beneficio.service';
+
+//declare var $: any;
 
 @Component({
   selector: 'app-beneficio',
@@ -9,21 +12,55 @@ import { BeneficioService } from '../beneficio.service';
 })
 export class BeneficioComponent implements OnInit {
 
-  mask = [' ', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, ',', /\d/, /\d/];
-  //mask = ['R$ ', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, ',', /\d/, /\d/ ];
+  //datepickerConfig: Partial<BsDatepickerConfig>;
+
   beneficio: Beneficio = new Beneficio(0, 0, "", 0, 0);
   message: any;
+  colorStyle: any;
 
-  constructor(private service: BeneficioService) { }
+  bsValue: Date = new Date();
+  minDate: Date = new Date();
 
-  ngOnInit() {
+  //constructor(private service: BeneficioService, private bsLocaleService: BsLocaleService) {
+  constructor(private service: BeneficioService) {
+
+    /*this.datepickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: false,
+        dateInputFormat: 'DD/MM/YYYY'
+      });*/
+
   }
+
+  ngOnInit() { }
 
   public adicionarBeneficio() {
     let resp = this.service.adicionarBeneficio(this.beneficio);
     //resp.subscribe((data) => this.message = data);
     resp = this.service.adicionarBeneficio(this.beneficio);
     console.log(resp);
+  }
+
+
+  onOptionsSelected(event) {
+    let selectComp = event.target;
+
+    this.colorStyle = "#495057";
+
+    console.log(selectComp.value);
+  }
+
+  onCheckRadiosSelected(label){
+
+    label.colorStyle = "green";
+
+    console.log(label);
+  }
+
+  onPrint() {
+    document.title = "Benefício Flex - Cremesp";
+    window.print();
   }
 
 }
